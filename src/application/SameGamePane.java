947f7226777382cfe2319c0c70144ed23f971a04
Fabian Waltermann <waltermann.fabian@fh-swf.de>
2022-06-20 16:47:24 +0200

    SameGameCanvas Class

--- src/application/SameGamePane.java
+++ src/application/SameGamePane.java
@@ -2,7 +2,7 @@ package application;
 
 import java.util.Optional;
 
-import javafx.scene.canvas.Canvas;
+import javafx.application.Platform;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -10,33 +10,19 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
-import samegame.common.Constants;
 import samegame.model.SameGameBoard;
 
 public class SameGamePane extends BorderPane
 {
    private SameGameBoard board;   
-   private Canvas canvas;
+   private SameGameCanvas canvas;
    private Label blockCount = new Label();
-   
-   private final Color [] colors = new Color[]
-   {
-             Color.BLACK,
-             Color.RED,
-             Color.BLUE,
-             Color.GREEN
-         
-   };
-
-
-   private GraphicsContext context;
    
    public SameGamePane()
    {
       board = new SameGameBoard();
       
-      
-      canvas = new Canvas(Constants.SCREEN_DIMENSION[0],Constants.SCREEN_DIMENSION[1]);
+      canvas = new SameGameCanvas(board);
       
       canvas.setOnMouseClicked(event ->
       {
@@ -50,7 +36,7 @@ public class SameGamePane extends BorderPane
          
          board.deleteBlocks(row, column);
          
-         render();
+         canvas.renderCanvas();
          
          updateLabel();
          
@@ -62,62 +48,40 @@ public class SameGamePane extends BorderPane
       });
       
       setCenter(canvas);
-      setBottom(blockCount);
+      setBottom(blockCount);      
+      canvas.renderCanvas();
       
-      context = canvas.getGraphicsContext2D();
-      
-      render();
       updateLabel();
    }
 
    private void endGame()
    {
-      Alert popup = new Alert(AlertType.INFORMATION);
+      Alert popup = new Alert(AlertType.CONFIRMATION);
       popup.setTitle("Game over!");
-      if(board.getRemaining() != 0)
+      
+      //Game won or lost?
+      if(board.getRemaining() > 0)
       {
          popup.setHeaderText("Sie haben verloren.");
-         popup.setContentText("Es sind noch "+ board.getRemaining() +" Blöcke über! \nUm nocheinmal zu spielen drücken sie \"OK\"");
+         popup.setContentText("Es sind noch "+ board.getRemaining() +" Blöcke über! \nUm nocheinmal zu spielen drücken sie \"OK\" \nUm das Programm zu beenden drücken sie \"Abbrechen\" ");
       }
       else
       {
          popup.setHeaderText("Sie haben gewonnen.");
-         popup.setContentText("Alle blöcke wurden entfernt! \nUm nocheinmal zu spielen drücken sie \"OK\" ");
+         popup.setContentText("Alle blöcke wurden entfernt! \nUm nocheinmal zu spielen drücken sie \"OK\" \nUm das Programm zu beenden drücken sie \"Abbrechen\" ");
       }
+      
+      // Check for Newgame or Exit
       Optional<ButtonType> result = popup.showAndWait();
       if(result.get() == ButtonType.OK)
       {
          this.board =  new SameGameBoard();
-         render();
+         canvas.setBoard(board);
+         canvas.renderCanvas();
       }
       else
       {
-         
-      }
-      
-   }
-
-   private void render()
-   {
-      context.setFill(Color.BLACK);
-      context.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-      
-      double width = canvas.getWidth() / board.getColumns();
-      
-      double height = canvas.getHeight() / board.getRows();
-      
-      for(int col = 0; col < board.getColumns(); col++)
-      {
-         for(int row = 0; row < board.getRows(); row++)
-         {
-            int block = board.getBoardSpace(row, col);
-            
-            if(block > 0)
-            {
-               context.setFill(colors[block]);
-               context.fillRect( (col*width) , (row*height) , height, width );
-            }
-         }
+         Platform.exit();
       }
       
    }
